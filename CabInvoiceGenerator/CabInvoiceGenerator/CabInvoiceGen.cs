333e1d8418c13bec1ef07008a2f9d4181c1e8059
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-13 00:29:19 +0530

    [Shivaraj] Added Calculating Fare In Cab Invoice Generator

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/CabInvoiceGen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class CabInvoiceGen
+    {
+        // If Variable is read only values can be only changed in constructor.
+        public readonly int MINIMUM_FARE;
+        public readonly int COST_PER_KM;
+        public readonly int COST_PER_MINUTE;
+
+        // Parameterized constructor
+        public CabInvoiceGen(RideType type)
+        {
+            if (type.Equals(RideType.NORMAL))
+            {
+                MINIMUM_FARE = 5;
+                COST_PER_KM = 10;
+                COST_PER_MINUTE = 1;
+            }
+        }
+
+        // UC1 - Method to calculate fare for single ride
+        public double CalculateFare(int time, double distance)
+        {
+            double totalFare;
+            try
+            {
+                if (time <= 0)
+                    throw new CabInvoiceGeneratorException(CabInvoiceGeneratorException.ExceptionType.INVALID_TIME, "Invalid Time");
+                if (distance <= 0)
+                    throw new CabInvoiceGeneratorException(CabInvoiceGeneratorException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
+                //Total fare for single ride
+                totalFare = (distance * COST_PER_KM) + (time * COST_PER_MINUTE);
+                //Comparing minimum fare and calculated fare to return the maximum fare
+                return Math.Max(totalFare, MINIMUM_FARE);
+            }
+            catch (CabInvoiceGeneratorException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
+
+//UC-1
+//Given a distance and time, the invoice generator shoud return total fare of the journey
+//cost - Rs. 10 Per Kilo meter + Rs. 1 Per Minute
+//Min Fare Rs - 5.
+
+//CabInvoicGeneratorTest
+//  Tests in group: 2
+
+//  Total Duration: 3.1 min
+
+//Outcomes
+//   2 Passed
+
--- CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Cab Invoice Generator Problem");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Cab Invoice Generator Program");
+
+            CabInvoiceGen cabInvoiceGenerator = new CabInvoiceGen(RideType.NORMAL);
+            Console.WriteLine(cabInvoiceGenerator.CalculateFare(10, 15));
+            Console.ReadLine();
+        }
+    }
+}
